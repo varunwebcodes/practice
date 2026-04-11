ae8c3e3e777db1c8bf29df2a6031a260b4c59796
Varun Negi <varunnegiweb@gmail.com>
2026-04-11 19:09:45 +0530

    Add protected Route

--- Backend/src/routes/users.route.js
+++ Backend/src/routes/users.route.js
@@ -85,4 +85,13 @@ router.post('/login', async(req,res)=>{
     }
 });
 
+router.get('/profile', authMiddleware, async(req,res)=>{
+    try{
+        const user = await User.findById(req.user.id).select('-password');
+        res.status(200).json({ user })
+    }catch(error){
+        res.status(500).json({error: error.message})
+    }
+})
+
 module.exports = router ;
